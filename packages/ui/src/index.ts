6f72120cae6a84bd4e5dacfdf54ee56a902814f1
zhixinghy <115979464+zhixinghy@users.noreply.github.com>
2026-07-22 06:53:36 +0800

    feat(ui): add shadcn ui components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,5 @@
-export { Button } from './components/ui/button'
+export * from './components/ui/button';
+export * from './components/ui/card';
+export * from './components/ui/dialog';
+export * from './components/ui/input';
+export * from './components/ui/textarea';
